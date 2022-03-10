42f70762b5bf7056ad8e3d0bfda052b301ee9e07
hao <manonghao@gmail.com>
2022-03-10 17:02:35 +0800

    Update ReadingMode.js

--- ReadingMode.js
+++ ReadingMode.js
@@ -3,7 +3,7 @@
 // @namespace    jianshu
 // @version      0.1
 // @description  去除文章页面的头尾、右侧边栏（作者信息）和左侧点赞等按钮，只保留正文
-// @author       elecrabbit
+// @author       zh
 // @match        *://www.jianshu.com/p/*
 // @require      http://cdn.bootcss.com/jquery/1.8.3/jquery.min.js
 // @grant        none
